7b5152debc1626701a6830305aebe10d723dfcdd
MustafaGuer <mustafaguer@outlook.com>
2023-01-15 04:12:20 +0100

    add: components && consts

---
+++ src/shared/consts/expenses.js
@@ -0,0 +1,21 @@
+export const expenses = [
+    {
+        id: 'e1',
+        title: 'Headset',
+        amount: 94.12,
+        date: new Date(2023, 0, 13),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 11, 12) },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2023, 0, 15),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2023, 0, 12),
+    },
+];
